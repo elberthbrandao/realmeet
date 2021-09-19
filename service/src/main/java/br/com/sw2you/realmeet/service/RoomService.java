284501805ed5ee408512be154463f05fb8c525cf
elberthbrandao <zelltr@hotmail.com>
2021-09-19 16:33:43 -0300

    Implementado getRoom, corrigido api.yml, feito mapStruct para RoomDTO

--- service/src/main/java/br/com/sw2you/realmeet/service/RoomService.java
+++ service/src/main/java/br/com/sw2you/realmeet/service/RoomService.java
@@ -2,8 +2,10 @@ package br.com.sw2you.realmeet.service;
 
 import static java.util.Objects.requireNonNull;
 
+import br.com.sw2you.realmeet.api.model.RoomDTO;
 import br.com.sw2you.realmeet.domain.entity.Room;
 import br.com.sw2you.realmeet.domain.pository.RoomRepository;
+import br.com.sw2you.realmeet.mapper.RoomMapper;
 import java.util.Objects;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -12,13 +14,16 @@ import org.springframework.stereotype.Service;
 public class RoomService {
 
     private final RoomRepository roomRepository;
+    private final RoomMapper roomMapper;
 
-    public RoomService(RoomRepository roomRepository) {
+    public RoomService(RoomRepository roomRepository, RoomMapper roomMapper) {
         this.roomRepository = roomRepository;
+        this.roomMapper = roomMapper;
     }
 
-    public Room findById(Long id) {
+    public RoomDTO getRoom(Long id) {
         requireNonNull(id);
-        return roomRepository.findById(id).orElseThrow(RuntimeException::new);
+        Room room = roomRepository.findById(id).orElseThrow(RuntimeException::new);
+        return roomMapper.fromEntityToDTO(room);
     }
 }
