a578a6839bae90c37a2904489cb02c0942b811b1
elberthbrandao <zelltr@hotmail.com>
2021-10-04 20:59:31 -0300

    Correção api.yml e finalizado a estrutura para suporte a testes de integração

--- service/src/test/java/br/com/sw2you/realmeet/core/BaseIntegrationTest.java
+++ service/src/test/java/br/com/sw2you/realmeet/core/BaseIntegrationTest.java
@@ -2,8 +2,11 @@ package br.com.sw2you.realmeet.core;
 
 import br.com.sw2you.realmeet.Application;
 import br.com.sw2you.realmeet.api.ApiClient;
+import java.net.MalformedURLException;
+import java.net.URL;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.test.context.ActiveProfiles;
 import org.flywaydb.core.Flyway;
 import org.junit.jupiter.api.BeforeEach;
@@ -15,6 +18,9 @@ public abstract class BaseIntegrationTest {
     @Autowired
     private Flyway flyway;
 
+    @LocalServerPort
+    private int serverPort;
+
     @BeforeEach
     void setup() {
         setupFlyway();
@@ -23,8 +29,8 @@ public abstract class BaseIntegrationTest {
 
     protected void setupEach() {}
 
-    protected void setLocalHostBasePath(ApiClient apiClient, String path) {
-
+    protected void setLocalHostBasePath(ApiClient apiClient, String path) throws MalformedURLException {
+        apiClient.setBasePath(new URL("http", "localhost", serverPort, path).toString());
     }
 
     private void setupFlyway() {
