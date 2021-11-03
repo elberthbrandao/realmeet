716d52d12dc2ad38abe03ba92d894d4a2775e5c3
elberthbrandao <zelltr@hotmail.com>
2021-11-03 17:11:59 -0300

    Atualizando api.yml

--- service/src/test/java/br/com/sw2you/realmeet/unit/RoomServiceUnitTest.java
+++ service/src/test/java/br/com/sw2you/realmeet/unit/RoomServiceUnitTest.java
@@ -1,7 +1,5 @@
 package br.com.sw2you.realmeet.unit;
 
-import static br.com.sw2you.realmeet.utils.MapperUtils.roomMapper;
-import static br.com.sw2you.realmeet.utils.TestDataCreator.*;
 import static br.com.sw2you.realmeet.utils.TestDataCreator.newRoomBuilder;
 import static br.com.sw2you.realmeet.utils.TestsConstants.DEFAULT_ROOM_ID;
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -11,17 +9,12 @@ import static org.mockito.Mockito.when;
 import br.com.sw2you.realmeet.core.BaseUnitTest;
 import br.com.sw2you.realmeet.domain.pository.RoomRepository;
 import br.com.sw2you.realmeet.exception.RoomNotFoundException;
-import br.com.sw2you.realmeet.mapper.RoomMapper;
 import br.com.sw2you.realmeet.service.RoomService;
 import br.com.sw2you.realmeet.utils.MapperUtils;
-import br.com.sw2you.realmeet.utils.TestDataCreator;
 import java.util.Optional;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
-import org.mockito.Mockito;
-import org.mockito.junit.jupiter.MockitoExtension;
 
 class RoomServiceUnitTest extends BaseUnitTest {
     private RoomService victim;
