c32c0800a6bba286b6655cb670fd43c40c34d167
elberthbrandao <zelltr@hotmail.com>
2021-11-05 18:42:34 -0300

    Usando o RoomMapper para mapear objetos e criando as classes para representar erros de validação

---
+++ service/src/main/java/br/com/sw2you/realmeet/validator/ValidationErrors.java
@@ -0,0 +1,44 @@
+package br.com.sw2you.realmeet.validator;
+
+import java.util.Iterator;
+import java.util.List;
+import org.springframework.data.util.Streamable;
+
+public class ValidationErrors implements Streamable<ValidationError> {
+    private final List<ValidationError> validationErrorList;
+
+    public ValidationErrors(List<ValidationError> validationErrorList) {
+        this.validationErrorList = validationErrorList;
+    }
+
+    public ValidationErrors add(String field, String errorCode) {
+        return add(new ValidationError(field, errorCode));
+    }
+
+    public ValidationErrors add(ValidationError validationError) {
+        validationErrorList.add(validationError);
+        return this;
+    }
+
+    public ValidationError getError(int index) {
+        return validationErrorList.get(index);
+    }
+
+    public int getNumberOfErrors() {
+        return validationErrorList.size();
+    }
+
+    public boolean hasErrors() {
+        return !validationErrorList.isEmpty();
+    }
+
+    @Override
+    public String toString() {
+        return "ValidationErrors{ validationErrorList=" + validationErrorList + '}';
+    }
+
+    @Override
+    public Iterator<ValidationError> iterator() {
+        return validationErrorList.iterator();
+    }
+}
