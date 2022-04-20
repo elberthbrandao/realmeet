2db6cd822a69ecf7c68124d46750b7474e94b3c2
elberthbrandao <zelltr@hotmail.com>
2022-04-19 22:05:38 -0300

    Corrigindo os testes de integração

--- service/src/test/java/br/com/sw2you/realmeet/core/BaseIntegrationTest.java
+++ service/src/test/java/br/com/sw2you/realmeet/core/BaseIntegrationTest.java
@@ -1,13 +1,22 @@
 package br.com.sw2you.realmeet.core;
 
+import static br.com.sw2you.realmeet.utils.TestsConstants.TEST_CLIENT_API_KEY;
+import static org.mockito.BDDMockito.given;
+
 import br.com.sw2you.realmeet.Application;
 import br.com.sw2you.realmeet.api.ApiClient;
+import br.com.sw2you.realmeet.domain.entity.Client;
+import br.com.sw2you.realmeet.domain.repository.ClientRepository;
+import br.com.sw2you.realmeet.utils.TestsConstants;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.Optional;
 import org.flywaydb.core.Flyway;
 import org.junit.jupiter.api.BeforeEach;
+import org.mockito.BDDMockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.test.context.ActiveProfiles;
 
@@ -20,9 +29,13 @@ public abstract class BaseIntegrationTest {
     @LocalServerPort
     private int serverPort;
 
+    @MockBean
+    private ClientRepository clientRepository;
+
     @BeforeEach
     void setup() throws Exception {
         setupFlyway();
+        mockApiKey();
         setupEach();
     }
 
@@ -35,5 +48,19 @@ public abstract class BaseIntegrationTest {
     private void setupFlyway() {
         flyway.clean();
         flyway.migrate();
+    }
+
+    private void mockApiKey() {
+        given(clientRepository.findById(TEST_CLIENT_API_KEY))
+            .willReturn(
+                Optional.of(
+                    Client
+                        .newBuilder()
+                        .apiKey(TEST_CLIENT_API_KEY)
+                        .description(TestsConstants.TEST_CLIENT_DESCRIPTION)
+                        .active(true)
+                        .build()
+                )
+            );
     }
 }
