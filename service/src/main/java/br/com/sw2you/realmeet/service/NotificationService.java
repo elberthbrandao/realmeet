978688b447bb44c92cafc7e31d2cecc2ec16a80f
elberthbrandao <zelltr@hotmail.com>
2022-05-03 21:09:02 -0300

    Escrevendo os testes de integração do NotificationService

--- service/src/main/java/br/com/sw2you/realmeet/service/NotificationService.java
+++ service/src/main/java/br/com/sw2you/realmeet/service/NotificationService.java
@@ -1,15 +1,18 @@
 package br.com.sw2you.realmeet.service;
 
+import static br.com.sw2you.realmeet.util.Constants.ALLOCATION;
+
 import br.com.sw2you.realmeet.domain.entity.Allocation;
 import br.com.sw2you.realmeet.email.EmailInfoBuilder;
 import br.com.sw2you.realmeet.email.EmailSender;
 import br.com.sw2you.realmeet.email.TemplateType;
+import br.com.sw2you.realmeet.util.Constants;
 import java.util.Map;
 import org.springframework.stereotype.Service;
 
 @Service
 public class NotificationService {
-    private static final String ALLOCATION = "allocation";
+
     private final EmailSender emailSender;
     private final EmailInfoBuilder emailInfoBuilder;
 
